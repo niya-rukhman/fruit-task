2eef52205d412206b13ec00f07f34ee2a25c1f45
niya-rukhman <49750829+niya-rukhman@users.noreply.github.com>
2019-06-30 14:10:32 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,135 @@
- const costOfApple = 10;
- const costOfOrange = 15;
- const costOfAppleBanana = 7;
- let costumerHasRs = 0;
- let noOfApple = 0;
- let noOfOrange = 0;
- let noOfBanana = 0;
- let totalCost = 0;
-let customerHasRs = 27;
+const costOfApple = 10;
+const costOfOrange = 15;
+const costOfBanana = 7;
+let noOfApple = 0;
+let noOfOrange = 0;
+let noOfBanana = 0;
+let totalCost = 0;
+let totalCostEl = document.getElementById("totalAmount");
+totalCost = noOfApple * costOfApple + noOfOrange * costOfOrange + noOfBanana * costOfBanana;
 
-function plusapple(){
-    let inputValue =  (document.querySelector("#buyapple").value);
-   noOfApple = 2;
-   totalCost = 20;
-   totalCost = customerHasRs -  totalCost;
- if(customerHasRs < 10){
+function cashValue(){ 
+ let userAmount = document.getElementById("totalRs").value;
+     
+//  if(userAmount < totalCostEl.innerHTML){     
+
+//     alert("Sorry,you have not enough money.");
+   
+//    }
+//            else{
+//                alert("Shopping SUCCESS!!");
+//     }
+}   
+    
+
+function plusApple() {
+    let addApple = ++noOfApple;
+    document.getElementById('buyApple').value = addApple;
+    console.log(addApple);
+    totalCostEl.innerHTML = noOfApple * costOfApple + noOfOrange * costOfOrange + noOfBanana * costOfBanana;
+    
+     if(userAmount > totalCostEl){     
+
      alert("Sorry,you have not enough money.");
- }
- else{
-     alert("Shopping SUCCESS!!");
- }
+    
+    }
+            else{
+                alert("Shopping SUCCESS!!");
+     }
+    
+    
 }
 
+function minusApple() {
+    let subApple = --noOfApple;
+    document.getElementById("buyApple").value = subApple;
+    console.log(subApple);
+    totalCostEl.innerHTML = noOfApple * costOfApple + noOfOrange * costOfOrange + noOfBanana * costOfBanana;
+
+    if(userAmount < totalCost){     
+
+     alert("Sorry,you have not enough money.");
+    
+    }
+            else{
+                alert("Shopping SUCCESS!!");
+     }
+    
+}
+  
+function plusOrange() {
+    let addOrange = ++noOfOrange;
+    document.getElementById('buyOrange').value = addOrange;
+    console.log(addOrange);
+    totalCostEl.innerHTML = noOfApple * costOfApple + noOfOrange * costOfOrange + noOfBanana * costOfBanana;
+    
+    if(userAmount < totalCost){     
+
+     alert("Sorry,you have not enough money.");
+    
+    }
+            else{
+                alert("Shopping SUCCESS!!");
+     }
+    
+}
+
+function minusOrange() {
+    let subOrange = --noOfOrange;
+    document.getElementById("buyOrange").value = subOrange;
+    console.log(subOrange);
+    totalCostEl.innerHTML = noOfApple * costOfApple + noOfOrange * costOfOrange + noOfBanana * costOfBanana;
+
+    if(userAmount < totalCost){     
+
+        alert("Sorry,you have not enough money.");
+       
+   }
+           else{
+               alert("Shopping SUCCESS!!");
+    }
+    
+    
+}
+  
+function plusBanana() {
+    let addBanana = ++noOfBanana;
+    document.getElementById('buyBanana').value = addBanana;
+    console.log(addBanana);
+    totalCostEl.innerHTML = noOfApple * costOfApple + noOfOrange * costOfOrange + noOfBanana * costOfBanana;
+    
+    if(userAmount < totalCost){     
+
+     alert("Sorry,you have not enough money.");
+    
+    }
+            else{
+                alert("Shopping SUCCESS!!");
+     }
+    
+    
+}
+
+function minusBanana() {
+    let subBanana = --noOfBanana;
+    document.getElementById("buyBanana").value = subBanana;
+    console.log(subBanana);
+    totalCostEl.innerHTML = noOfApple * costOfApple + noOfOrange * costOfOrange + noOfBanana * costOfBanana;
+
+    if(userAmount < totalCost){     
+
+        alert("Sorry,you have not enough money.");
+       
+   }
+           else{
+               alert("Shopping SUCCESS!!");
+    }
+    
+}
+  
 
 
 
-{/* <button class="add-qty" onclick="plusapple()"><i class="fa fa-plus"></i></button>
-       <input id="buyapple" type="number" min="0">
-        <button class="sub-qty" onclick="minusapple()"><i class="fa fa-minus"></i></button>
-        </div> */}
+
+
+
